264823ce28d0c540160a255260f6cb6d8f743bd7
mateo guevara <mateo.guevara@thecocktail.com>
2021-04-27 19:25:18 -0500

    heroku deploy url added

--- src/components/chat-room/ChatRoom.tsx
+++ src/components/chat-room/ChatRoom.tsx
@@ -14,7 +14,7 @@ function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const ENDPOINT = "localhost:5000";
+const ENDPOINT = "https://interactive-chatroom.herokuapp.com";
 const socket: Socket<DefaultEventsMap, DefaultEventsMap> = io(ENDPOINT, {
   upgrade: false,
   transports: ["websocket"],
